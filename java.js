56cbee5659c36c9b3b91ab11a3af8ddcc0489425
Gabriel <gabriel.touzalin@gmail.com>
2022-02-15 21:19:31 +0100

    forgot to add files so here it is

--- java.js
+++ java.js
@@ -1,5 +1,48 @@
+const buttons = document.querySelectorAll(".btn").forEach((item) => {
+    item.addEventListener("click", () => {
+        handleclick(item.id);
+        console.log(item.id);
+    });
+});
+const score = document.getElementById("score");
+
+let i = 0;
+let computerCount = 0;
+let playerCount = 0;
+
+score.innerText = playerCount + " - " + computerCount;
+
+function handleclick(playerSelection) {
+    if (playerCount + computerCount >= 5) {
+        if (playerCount > computerCount) {
+            score.innerText = "You win...";
+            return;
+        } else if (playerCount < computerCount) {
+            score.innerText = "Better luck next time!";
+            return;
+        } else if (playerCount == computerCount) {
+            score.innerText = "A draw that suck.....";
+            return;
+        }
+    } else {
+        temp = playRound(playerSelection, computerPlay());
+        if (temp[0] == 1) {
+            playerCount++;
+        } else if (temp[0] == 0) {
+            computerCount++;
+        } else {
+            playerCount++;
+            computerCount++;
+        }
+        score.innerText = playerCount + " - " + computerCount;
+        if (playerCount + computerCount >= 5) {
+            handleclick(playerSelection);
+        }
+    }
+}
+
 function computerPlay() {
-    var moves = ["rock", "paper", "scissors"];
+    var moves = ["rock", "penguin", "salmon"];
     var moves = moves[Math.floor(Math.random() * moves.length)];
     return moves;
 }
@@ -8,74 +51,34 @@ function playRound(playerSelection, computerSelection) {
         case playerSelection === computerSelection:
             return [2, "égalité"];
             break;
-        case playerSelection === "rock" && computerSelection === "scissors":
-            return (
-                [1, "You win! " + playerSelection + " beats " + computerSelection]
-            );
+        case playerSelection === "salmon" && computerSelection === "rock":
+            return [1, "You win! " + playerSelection + " beats " + computerSelection];
             break;
-        case playerSelection === "rock" && computerSelection === "paper":
-            return (
-                [0, "You lose! " + computerSelection + " beats " + playerSelection]
-            );
+        case playerSelection === "penguin" && computerSelection === "rock":
+            return [
+                0,
+                "You lose! " + computerSelection + " beats " + playerSelection,
+            ];
             break;
-        case playerSelection === "scissors" && computerSelection === "rock":
-            return (
-                [0, "You lose! " + playerSelection + " beats " + computerSelection]
-            );
+        case playerSelection === "rock" && computerSelection === "salmon":
+            return [
+                0,
+                "You lose! " + computerSelection + " beats " + playerSelection,
+            ];
             break;
-        case playerSelection === "scissors" && computerSelection === "paper":
-            return (
-                [1, "You win! " + computerSelection + " beats " + playerSelection]
-            );
+        case playerSelection === "penguin" && computerSelection === "salmon":
+            return [1, "You win! " + playerSelection + " beats " + computerSelection];
             break;
-        case playerSelection === "paper" && computerSelection === "rock":
-            return (
-                [1, "You win! " + playerSelection + " beats " + computerSelection]
-            );
+        case playerSelection === "rock" && computerSelection === "penguin":
+            return [1, "You win! " + playerSelection + " beats " + computerSelection];
             break;
-        case playerSelection === "paper" && computerSelection === "scissors":
-            return (
-                [0, "You lose! " + computerSelection + " beats " + playerSelection]
-            );
+        case playerSelection === "salmon" && computerSelection === "penguin":
+            return [
+                0,
+                "You lose! " + computerSelection + " beats " + playerSelection,
+            ];
             break;
 
         default:
     }
 }
-/*
-function game() {
-    let i;
-    let computerCount = 0;
-    let playerCount = 0;
-    for (let i = 0; i <= 5; i++) {
-        const computerSelection = computerPlay();
-        const playerSelection = playerPlay();
-        temp = playRound(playerSelection, computerSelection);
-        if (temp[0] == 1) {
-            playerCount++;
-        } else if (temp[0] == 0) {
-            computerCount++;
-        } else { }
-        console.log(temp[1], playerCount, computerCount);
-    }
-    if (playerCount > computerCount) {
-        console.log("You win...");
-    } else { console.log("Better luck next time!"); }
-
-
-
-}
-*/
-function playerPlay(){
-    const buttons = document.querySelectorAll('.btn').forEach(item => {
-        item.addEventListener('click', () => {
-          alert(item);
-        });
-      });
-}
-
-const buttons = document.querySelectorAll('.btn').forEach(item => {
-    item.addEventListener('click', () => {
-        alert(item.id);
-    });
-  });
